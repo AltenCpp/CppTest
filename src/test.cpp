ba765b314e2c524d856af3375adc8d8d3976abfc
plerenius <plerenius@gmail.com>
2022-03-31 21:04:06 +0200

    Added test cases
    Also made minor updates to ouptut and script

--- src/test.cpp
+++ src/test.cpp
@@ -14,18 +14,33 @@ bool testEq(std::string title,Shape &a, Shape &b) {
     return res;
 }
 
+bool testNotEq(std::string title,Shape &a, Shape &b) {
+    bool res = false;
+    std::cout << "TEST NOT EQ: " << title;
+    if (a == b){
+        std::cout << "False" << std::endl;
+        res = false;
+    } else {
+        std::cout << "True" << std::endl;
+        res = true;
+    }
+    return res;
+}
+
 int main(){
     Rectangle rectA(2,4);
     Rectangle rectB(5,5);
+    Rectangle rectC(2,4);
     Square sq(5);
     Circle cirk(10);
     
     bool res=true;
-    res &= testEq("Test rectA==rectA: ",rectA,rectA);
-    res &= testEq("Test rectB==rectB: ",rectB,rectB);
-    res &= testEq("Test sq==rectB: ",sq,rectB);
-    res &= !testEq("Test sq==cirk: ",sq,cirk);
-    res &= !testEq("Test rectA==rectB: ",rectA,rectB);
+    res &= testEq("Test rectA == rectA: ",rectA,rectA);
+    res &= testEq("Test rectB == rectB: ",rectB,rectB);
+    res &= testEq("Test rectA == rectC: ",rectA,rectC);
+    res &= testEq("Test rectB == sq: ",rectB,sq);
+    res &= testNotEq("Test cirk != sq: ",cirk,sq);
+    res &= testNotEq("Test rectA != rectB: ",rectA,rectB);
     
     return (res) ? 0 : 1;
 }
--- testScript.sh
+++ testScript.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-g++ src/test.cpp src/polygon.cpp -o test.out && ./test.out
+if (g++ src/test.cpp src/polygon.cpp -o test.out && ./test.out)
+then
+  echo "----------------------"
+  echo "Tests SUCCESSFUL"
+else
+  echo "----------------------"
+  echo "Tests FAILED"
+fi
